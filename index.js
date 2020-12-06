68cb619f6542c1a367afdc1352a71acf5d86780d
aletson <me@ajl.io>
2020-12-06 13:30:03 -0500

    add finalhandler

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 var http = require('http');
 var Router = require('router');
+var finalhandler = require('finalhandler');
 
 client.login(process.env.app_token)
 
